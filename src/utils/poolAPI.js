85c0f4f942492de2af48118659a8750d90599cd8
SprengerV <B.Francis@SprengerV.com>
2021-04-30 19:41:25 +0000

    api call to ltcpool

--- src/utils/poolAPI.js
+++ src/utils/poolAPI.js
@@ -2,13 +2,24 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const baseURL = 'https://api.ethermine.org'
-const minerURL = baseURL + '/miner/' + process.env.REACT_APP_ETH_ADDR
+
+const ethURL = 'https://api.ethermine.org';
+const ethMinerURL = ethURL + '/miner/' + process.env.REACT_APP_ETH_ADDR;
+const ltcURL = 'https://litecoinpool.org/api';
+const ltcMinerURL = ltcURL + '?api_key=' + process.env.REACT_APP_LTC_API_KEY;
+const arrrURL = '';
+const arrrMinerURL = arrrURL + '';
 
 const poolAPI = {
   ethermine: function() {
-    return axios.get(minerURL + '/dashboard');
-  }
+    return axios.get(ethMinerURL + '/dashboard');
+  },
+  ltcpool: function() {
+    return axios.get(ltcMinerURL);
+  },
+  coolmine: function() {
+    return axios.get(arrrMinerURL);
+  }  
 };
 
 export default poolAPI;
