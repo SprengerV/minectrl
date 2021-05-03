c1b150b06a989bb3509e63b565ab1cdd42f19e34
SprengerV <B.Francis@SprengerV.com>
2021-05-03 07:44:38 +0000

    iframe for coolmine add since they don't have a public api

--- src/components/coolmine/index.js
+++ src/components/coolmine/index.js
@@ -1,41 +1,48 @@
 import React, { useEffect, useState } from 'react';
+import dotenv from 'dotenv';
+import * as dayjs from 'dayjs';
+import './coolmine.css'
+dotenv.config();
 
-function coolmine() {
-  const [dash, setDash] = useState({});
-  const [count, setCount] = useState(0);
+function CoolMine() {
+  //const [dash, setDash] = useState({});
+  //const [count, setCount] = useState(0);
   const [update, setUpdate] = useState('never')
+  const coolmineURL = 'https://coolmine.top/miner.php?coin=1&wallet=' + process.env.REACT_APP_ARRR_ADDR;   
+  const reload = () => {
+    document.getElementById('coolmine').src += '';
+    setUpdate(dayjs().format('M/D/YYYY @h:mma'));
+  };
+  useEffect(() => {
+    const timer = setInterval(() => reload(), 120000);
+    return () => clearInterval(timer);
+  });
 
-  return (
+  return (<>
+    
     <div className="row">
       <div className="col-10 ms-auto me-auto container card">
         <div className="row d-flex flex-row-reverse">
-          <div className="col-3 d-flex justify-content-end">
-            <p>Last updated: { update }</p>
+          <div className="col-2">
+            <p>Last Updated: { update }</p>
           </div>
-        </div>  
-        <div className="row d-flex justify-content-around">
+        </div>
+        <div className="row d-flex justify-content-between">
           <div className="col-3">
-            <a href="#!" target="_blank" rel="noreferrer">
-              <h3>CoolMine ARRR</h3>
+            <a href={ coolmineURL } target="_blank" rel="noreferrer">
+              <h3>CoolMine.top (ARRR)</h3>
             </a>
           </div>
-          <div className="col-3">
-            <h4>Workers:</h4>
-            <p>num</p>
-          </div>
-          <div className="col-3">
-            <h4>Current Hashrate:</h4>
-            <p>num</p>
-          </div>
-          <div className="col-3">
-            <h4>Unpaid Balance:</h4>
-            <p>num</p>
+          <div className="col-1">
+            <button className="btn btn-secondary" id="cmReload" onClick={ () => reload() }>Refresh</button>
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-
-        </div>  
-      </div>  
-    </div>  
-  )  
+          <iframe title="CoolMine" id="coolmine" src={ coolmineURL }></iframe>
+        </div>
+      </div>
+    </div>
+  </>); 
 }
+
+export default CoolMine;
